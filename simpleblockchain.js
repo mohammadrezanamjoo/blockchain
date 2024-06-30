04f524b2a11417be47f9401d68a8606582358cd4
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-06-30 18:00:36 +0330

    Update and rename simpleblockc to simpleblockchain.js

---
+++ simpleblockchain.js
@@ -0,0 +1,73 @@
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return CryptoJS.SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("BLOCK MINED: " + this.hash);
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/2023", "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Test the implementation
+const { SHA256 } = require('crypto-js');
+global.CryptoJS = require('crypto-js');
+
+let savjeeCoin = new Blockchain();
+console.log('Mining block 1...');
+savjeeCoin.addBlock(new Block(1, "01/06/2023", { amount: 4 }));
+
+console.log('Mining block 2...');
+savjeeCoin.addBlock(new Block(2, "01/07/2023", { amount: 10 }));
+
+console.log(JSON.stringify(savjeeCoin, null, 4));
+console.log('Blockchain valid? ' + savjeeCoin.isChainValid());
