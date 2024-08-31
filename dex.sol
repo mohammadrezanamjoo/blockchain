90c63956c46bee471ab26c64bc6f6a3c42ea1f51
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-08-31 20:02:57 +0330

    Update dex.sol

--- dex.sol
+++ dex.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 
 interface IERC20 {
     function transfer(address to, uint256 amount) external returns (bool);
+
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount)  returns (bool);
     function balanceOf(address account) external view returns (uint256);
