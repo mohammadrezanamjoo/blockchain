f1964b573b17b3fe33822ed18a342d71c50b7168
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-11-02 10:05:19 +0330

    Update borrow.sol

--- borrow.sol
+++ borrow.sol
@@ -7,6 +7,7 @@ function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 
     function approve(address spender, uint256 amount) external returns (bool);
+
     function balanceOf(address account) external view returns (uint256);
 }
 
