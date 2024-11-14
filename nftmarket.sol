251b643767595d183329c13a05724daa819baeda
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-11-14 20:10:17 +0330

    Update nftmarket.sol

--- nftmarket.sol
+++ nftmarket.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import  "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract NFTMarketplace is ERC721, Ownable {
     using SafeMath for uint256;
