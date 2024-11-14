8163e38ed7c78b951bc89a1f234d868b351573cf
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-11-14 20:09:43 +0330

    Update borrow.sol

--- borrow.sol
+++ borrow.sol
@@ -18,7 +18,7 @@ contract LendingPlatform {
     mapping(address => uint256) public collateralBalances;
 
     event CollateralDeposited (address indexed borrower, uint256 amount);
-    event LoanGranted(address indexed borrower, uint256 loanAmount);
+    event  LoanGranted(address indexed borrower, uint256 loanAmount);
     
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the contract owner can call this function");
